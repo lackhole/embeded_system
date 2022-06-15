a78ba12d08bfd79ed60f6b43050d9715c2f7eb5d
Tony <cosgenio@gmail.com>
2022-06-16 05:32:36 +0900

    add option listener

--- main.cc
+++ main.cc
@@ -52,7 +52,9 @@ std::optional<std::pair<std::string, std::string>> load_model_data(std::string u
 
   for (;;) {
     auto response = retry_get(3000, [&]() {
-      return protocol.Get(client, "model/ssd_mobilenet_v1_1_metadata_1.tflite");
+      return protocol.Get(client, "model/model.tflite");
+//      return protocol.Get(client, "model/ssd_mobilenet_v1_1_metadata_1.tflite");
+//      return protocol.Get(client, "model/yolov4-416-fp16.tflite");
     });
 
     auto it = response.find("data");
@@ -69,11 +71,14 @@ std::optional<std::pair<std::string, std::string>> load_model_data(std::string u
   for (;;) {
     auto response = retry_get(3000, [&]() {
       return protocol.Get(client, "model/labelmap.txt");
+//      return protocol.Get(client, "model/labelmap_ssd.txt");
+//      return protocol.Get(client, "model/labelmap_yolov4.txt");
     });
 
     auto it = response.find("data");
     if (it == response.end()) {
       watcher::Log.d("Failed to load labelmap");
+      std::this_thread::sleep_for(std::chrono::seconds(3));
       continue;
     }
 
@@ -84,18 +89,36 @@ std::optional<std::pair<std::string, std::string>> load_model_data(std::string u
   return std::make_pair(std::move(model_buffer), std::move(labelmap_buffer));
 }
 
-int main(int argc, char* argv[]) {
-  std::string url;
-  std::string port;
+std::optional<std::string> load_option(const std::string& url, const std::string& port, const std::string& option) {
+  watcher::TcpClient client(url, port);
+  watcher::Protocol protocol;
 
-  if (argc != 3) {
-    std::cerr << "Usage: [URL] [PORT]" << std::endl;
-    return EXIT_FAILURE;
+  try {
+    auto response = protocol.Get(client, "settings/" + option);
+
+
+    auto it = response.find("data");
+    if (it == response.end()) {
+      watcher::Log.d("Failed to load option ", option);
+    } else {
+      watcher::Log.d("Loaded option ", option, " : ", it->second);
+      auto r = std::move(it->second);
+      if (!r.empty() &&
+          (r[r.size() - 1] == '\r' || r[r.size() - 1] == '\n' ||
+          r[r.size() - 1] == ' ' || r[r.size() - 1] == '\t' ||
+          r[r.size() - 1] == '\0'))
+        r.erase(r.size() - 1);
+
+      return std::move(r);
+    }
+  } catch (const std::exception& e) {
+    watcher::Log.e(e.what());
   }
 
-  url = argv[1];
-  port = argv[2];
+  return std::nullopt;
+}
 
+bool run(const std::string& url, const std::string& port) {
   cv::Mat view;
   watcher::RingBuffer<cv::Mat> frames;
   std::atomic<bool> updated{false};
@@ -118,7 +141,7 @@ int main(int argc, char* argv[]) {
 
   std::mutex inference_result_m;
   watcher::MovementDetector::result_or_not inference_result;
-  detector.add_listener([&](const auto& result) {
+  boost::signals2::scoped_connection conn_detector = detector.add_listener([&](const auto& result) {
     std::lock_guard lck(inference_result_m);
     inference_result = result;
   });
@@ -131,7 +154,6 @@ int main(int argc, char* argv[]) {
 
   bool stop = false;
   bool pause = false;
-  int criteria = 50;
 
   const double scale = 1;
 
@@ -165,6 +187,10 @@ int main(int argc, char* argv[]) {
   auto conn = camera.add_listener(run_detection);
   camera.run();
 
+  std::mutex bbox_m;
+  std::vector<cv::Rect> bbox;
+  detector.bbox_.connect([&](const auto& b) { std::lock_guard lck(bbox_m); bbox = b; });
+
   while(!stop) {
     if (!pause) {
       if (bool expected = true; !updated.compare_exchange_strong(expected, false)) {
@@ -191,45 +217,50 @@ int main(int argc, char* argv[]) {
       }
       if (result_copy) {
         for (const auto& detection: *result_copy) {
-          if (detection.score < criteria * 0.01) continue;
-
           const cv::Point2f tl(detection.rect[1] * view.cols, detection.rect[0] * view.rows);
           const cv::Point2f br(detection.rect[3] * view.cols, detection.rect[2] * view.rows);
 
           cv::rectangle(view, tl, br, {255, 0, 0}, 2);
+
+          std::cerr << detection.label << std::endl;
 
           char buf[10];
           std::sprintf(buf, "(%.1f%%)", detection.score * 100);
           cv::putText(view,
                       detection.label + std::string(buf),
                       cv::Point2d(tl.x, tl.y - 4 * scale),
-                      cv::FONT_ITALIC, 1 * scale, {255, 255, 255}, 3);
+                      cv::FONT_ITALIC, 0.5 * scale, {255, 255, 255}, 2);
           cv::putText(view,
                       detection.label + std::string(buf),
                       cv::Point2d(tl.x, tl.y - 4 * scale),
-                      cv::FONT_ITALIC, 1 * scale, {0, 0, 0}, 2);
+                      cv::FONT_ITALIC, 0.5 * scale, {0, 0, 0}, 1);
         }
       }
     }
     const auto now = watcher::DateTime<>::now().time_zone(std::chrono::hours(9)).to_string();
 
-    text_criteria.text("Criteria: " + std::to_string(criteria));
+    text_criteria.text("Criteria: " + std::to_string(detector.score_threshold()));
     text_inference.text("Inference: " + std::to_string(detector.inference_time()) + "ms");
     text_fps.text("FPS: " + std::to_string(camera.fps()));
     text_time.text(now)
-             .org({5, view.rows - 30 * int(scale)});
+      .org({5, view.rows - 30 * int(scale)});
 
-    const auto mvs = detector.diffs_.load();
-    if (mvs) {
-//      cv::imshow("diff", *mvs);
-      for (const auto& rect : *mvs) {
-        cv::rectangle(view, cv::Point(rect.tl() / 2), cv::Point(rect.br()/2), {0,0,220}, 2);
-      }
+    decltype(bbox) bbox_copy;
+    {
+      std::lock_guard lck(bbox_m);
+      bbox_copy = bbox;
+    }
+    for (const auto& rect : bbox_copy) {
+      cv::rectangle(view, cv::Point(rect.tl() / 2), cv::Point(rect.br()/2), {0,0,220}, 2);
     }
 
     watcher::draw(view, text_criteria, text_inference, text_fps, text_time);
 
     video_client.feed(view, now, std::vector<std::string>());
+
+    if (auto o = load_option(url, port, "restart"); o && *o == "1") {
+      return true;
+    }
 
 # ifdef __APPLE__
     cv::imshow("Raspberry Pi", view);
@@ -245,11 +276,11 @@ int main(int argc, char* argv[]) {
           break;
 
         case '-':
-          criteria = std::max(0, criteria - 10);
+          detector.score_threshold(std::max(0.f, detector.score_threshold() - 0.1f));
           break;
 
         case '+':
-          criteria = std::min(100, criteria + 10);
+          detector.score_threshold(std::min(1.f, detector.score_threshold() + 0.1f));
           break;
       }
 //      generator->handle_key(key);
@@ -259,6 +290,28 @@ int main(int argc, char* argv[]) {
 
   conn.disconnect();
 
+  return false;
+}
+
+int main(int argc, char* argv[]) {
+  std::string url;
+  std::string port;
+
+  if (argc != 3) {
+    std::cerr << "Usage: [URL] [PORT]" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  url = argv[1];
+  port = argv[2];
+
+  while (true) {
+    if (run(url, port)) {
+      std::this_thread::sleep_for(std::chrono::seconds(5));
+      continue;
+    }
+    break;
+  }
 
   return 0;
 }
